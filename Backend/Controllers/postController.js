0823958fcfadba44859051b5110a65614a8ede4d
Chumexxx <obasyemeka@gmail.com>
2024-11-27 20:38:52 +0100

    delete post

--- Backend/Controllers/postController.js
+++ Backend/Controllers/postController.js
@@ -1,6 +1,6 @@
 const Post = require("../Models/postModel.js")
 const User = require("../Models/userModel.js")
-const cloudinary = require("cloudinary").v2
+// const cloudinary = require("cloudinary").v2
 
 const createPost = async (req, res) => {
     try{
@@ -37,9 +37,27 @@ const createPost = async (req, res) => {
 
 const deletePost = async (req, res) => {
     try{
+        const post = await Post.findById(req.params.id)
+        if(!post){
+            return res.status(404).json({ error: "Post not found" })
+        }
+
+        if(post.user.toString() !== req.user._id.toString()){
+            return res.status(401).json({ error: "You are not authorized to delete this post" })
+        }
+
+        // if(post.img){
+        //     const imgId = post.img.split("/").pop().split(".")[0]
+        //     await cloudinary.uploader.destroy(imgId)
+        // }
+
+        await Post.findByIdAndUpdate(req.params.id)
+
+        res.status(200).json({ message: "Post deleted successfully" })
 
     } catch (error){
-        
+        console.log("Error in deletePost controller ", error)
+        res.status(500).json({ error: "Internal server error"})
     }
 }
 
--- Backend/Routes/postRoute.js
+++ Backend/Routes/postRoute.js
@@ -6,6 +6,6 @@ const protectRoute = require("../Middleware/protectRoute.js")
 postRouter.post("/create", protectRoute, postController.createPost)
 // postRouter.post("/like/:id", protectRoute, postController.likeOrUnlikePost)
 // postRouter.post("/comment/:id", protectRoute, postController.commentOnPost)
-postRouter.post("/delete", protectRoute, postController.deletePost)
+postRouter.delete("/:id", protectRoute, postController.deletePost)
 
 module.exports = postRouter
